480401c18a6eadbc52a289571a74abdfd8d09c29
fuyicai <1400330380@qq.com>
2020-12-01 14:47:39 +0800

    添加移动、联通、电信切换提示

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { Modal } from 'ant-design-vue';
 
 Vue.use(Vuex)
+const  countDown = () => {
+  let secondsToGo = 3;
+  const modal = Modal.success({
+    title: '切换中,请稍后',
+    content: `资源加载中... ${secondsToGo} `,
+  });
+  const interval = setInterval(() => {
+    secondsToGo -= 1;
+    modal.update({
+      content: `资源加载中... ${secondsToGo} `,
+    });
+  }, 1000);
+  setTimeout(() => {
+    clearInterval(interval);
+    window.location.reload() ;
+    modal.destroy();
+  }, secondsToGo * 1000);
+};
 const master = window.sessionStorage.masterMapItem ? JSON.parse(window.sessionStorage.masterMapItem) : false ;
 export default new Vuex.Store({
   state: {
@@ -10,7 +29,7 @@ export default new Vuex.Store({
   mutations: {
     changMaster (state, n) {
       state.master = n ;
-      window.location.reload()
+      countDown()
     }
   },
   actions: {
